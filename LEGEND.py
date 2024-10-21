83b6c1b22ac862c317ec363ee04c649db356163f
Ayushddos002 <me.ayushdutta@gmail.com>
2024-10-22 00:34:39 +0530

    Update LEGEND.py

--- LEGEND.py
+++ LEGEND.py
@@ -6,8 +6,8 @@ from telegram.ext import Application, CommandHandler, MessageHandler, ContextTyp
 import pymongo
 
 # Configuration
-TOKEN = "7880546265:AAEyXvj5c5PTqfJ7Lx22o7pj5MPa4ckTjuU"  # Replace with your Telegram bot token
-ADMIN_IDS = {7372592479}  # Replace with your actual admin user ID(s)
+TOKEN = "7577329584:AAFchRwyJvmUfAz0osR8p8mUnGlG5xkm9Y4"  # Replace with your Telegram bot token
+ADMIN_IDS = {5215069840}  # Replace with your actual admin user ID(s)
 
 # MongoDB setup
 mongo_client = pymongo.MongoClient("mongodb+srv://Magic:Spike@cluster0.fa68l.mongodb.net/TEST?retryWrites=true&w=majority&appName=Cluster0")
